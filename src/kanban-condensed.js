9ad7396184b1aa5a0a102aa8f52e812e41a2c520
Jakub Rychecký <jakub.rychecky@mediafactory.cz>
2018-12-12 12:55:40 +0100

    Constants for configuration CSS

--- src/kanban-condensed.js
+++ src/kanban-condensed.js
@@ -1,41 +1,64 @@
 // ==UserScript==
 // @name         MEDIA FACTORY: JIRA Kanban Board Condensed
 // @namespace    http://jira.mediafactory.cz/
-// @version      0.1
+// @version      0.2
 // @description  Make your eyes *not* to bleed with new board.
 // @author       Jakub Rychecký <jakub@rychecky.cz>
-// @match        https://jira.mediafactory.cz/secure/RapidBoard.jspa?rapidView=96*
+// @match        *jira.mediafactory.cz/secure/RapidBoard.jspa?rapidView=96*
 // @grant        none
 // ==/UserScript==
 
 
 const hiddenColumnsIds = [836]; // Acceptance column
-const fixVersionColor = '#e30e0e'; // Color for Fix Version text
+
+const cssFixVersion = {
+    'color': '#E30E0E',
+    'font-weight': 'bold',
+};
+
+const cssLabels = {
+    'font-size': '9px',
+    'font-weight': 'bold',
+    'color': '#AAAAAA',
+};
+
+const cssEpic = {
+    'font-size': '9px',
+};
+
+const cssIssueSummary = {
+    'font-size': '13px',
+};
+
+const cssIssueContent = {
+    'padding': '5px 5px 5px 35px',
+};
+
+// General CSS for extra fields as time, labels, epic...
+const cssExtraFields = {
+    'margin-top': '5px',
+};
+
+// ----------------------------------
 
 
+(function () {
+    // Hide columns
+    hiddenColumnsIds.forEach((id) => {
+        $('.ghx-column[data-id=' + id + '], .ghx-column[data-column-id= ' + id + ']').hide();
+    });
 
-(function() {
+    // Header remove for more vertical space
+    $('#ghx-header').remove();
+
+
     $(document).ajaxComplete(() => {
-        // Hide columns
-        hiddenColumnsIds.forEach((id) => {
-            $('.ghx-column[data-id=' + id + '], .ghx-column[data-column-id= ' + id + ']').hide();
-        });
-
-
-
-        // Header remove for more vertical space
-        $('#ghx-header').remove();
-
-
-
         // EACH ISSUE EDITS
         $('.ghx-issue').each((i, element) => {
             const issue = $(element);
 
             // Change issue padding
-            issue.find('.ghx-issue-content').css({
-                'padding': '5px 5px 5px 35px',
-            });
+            issue.find('.ghx-issue-content').css(cssIssueContent);
 
 
             // Hide "None" rows
@@ -44,30 +67,22 @@ const fixVersionColor = '#e30e0e'; // Color for Fix Version text
 
                 if (row.text() === 'None') {
                     row.remove();
-                };
+                }
             });
 
 
             // Smaller issue text
-            issue.find('.ghx-summary').css({
-                'font-size': '13px',
-            });
+            issue.find('.ghx-summary').css(cssIssueSummary);
 
             // Smaller epic link
-            issue.find('.aui-label[data-epickey]').css({
-                'font-size': '9px',
-            });
+            issue.find('.aui-label[data-epickey]').css(cssEpic);
 
             // Remove logged time from card
             issue.find('[data-tooltip*="Progress"]').remove();
 
 
             // Change style of labels
-            issue.find('[data-tooltip^="Labels"]').css({
-                'font-size': '9px',
-                'font-weight': 'bold',
-                'color': '#AAAAAA',
-            });
+            issue.find('[data-tooltip^="Labels"]').css(cssLabels);
 
             // Normalize label case
             issue.find('[data-tooltip^="Labels"]').each((x, elementRow) => {
@@ -77,21 +92,11 @@ const fixVersionColor = '#e30e0e'; // Color for Fix Version text
 
             // Highligh for fix version
             issue.find('[data-tooltip^="Fix"]').each((x, elementRow) => {
-                $(elementRow).css({
-                    'color': fixVersionColor,
-                    'font-weight': 'bold',
-                });
+                $(elementRow).css(cssFixVersion);
             });
 
             // Margin of epic link
-            issue.find('.ghx-highlighted-fields').css({
-                'margin-top': '5px',
-            });
-
-            // Margin of labels
-            issue.find('.ghx-extra-fields').css({
-                'margin-top': '5px',
-            });
+            issue.find('.ghx-highlighted-fields, .ghx-extra-fields').css(cssExtraFields);
         });
 
     });
